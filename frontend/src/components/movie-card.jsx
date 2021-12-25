8ef2d4efe03f103dbfe3149be691fcdf5d637a8d
Axinion <55135463+Axinion@users.noreply.github.com>
2021-12-26 02:50:52 +0530

    final movie card
    finaalized movie card feature

--- frontend/src/components/movie-card.jsx
+++ frontend/src/components/movie-card.jsx
@@ -96,3 +96,103 @@ export default function MovieCard(props) {
             dispatch(setUser(res.data));
         });
     }
+
+    return props.movie ? (
+        <CSSTransition
+            in={true}
+            appear={true}
+            timeout={600}
+            classNames="fade"
+            unmountOnExit
+        >
+            <div className="movie-card">
+                <Card text="white">
+                    <div
+                        onClick={() => {
+                            cardOnClick();
+                        }}
+                    >
+                        <Card.Header>
+                            <Card.Img variant="top" src={props.movie.Poster} />
+                        </Card.Header>
+                        <Card.Body>
+                            <Card.Title
+                                className="poster-title"
+                                title={props.movie.Title}
+                            >
+                                {props.movie.Title}
+                            </Card.Title>
+
+                            <h5 className="card-text d-flex justify-content-between">
+                                <span
+                                    className="badge badge-warning h-100 badge-pill w-25"
+                                    title="IMDb Rating"
+                                >
+                                    {props.movie.imdbRating}
+                                </span>{' '}
+                                {props.movie.Metascore !== 'N/A' ? (
+                                    <span
+                                        className="badge badge-success h-100 badge-pill w-25"
+                                        title="Metascore"
+                                    >
+                                        {props.movie.Metascore}
+                                    </span>
+                                ) : (
+                                    ''
+                                )}
+                            </h5>
+                        </Card.Body>
+                    </div>
+                    <Card.Footer>
+                        <div className="d-flex justify-content-between">
+                            <button
+                                className={
+                                    isSaved === true
+                                        ? 'btn btn-info'
+                                        : 'btn btn-outline-info'
+                                }
+                                onClick={() => {
+                                    if (user.currentUser) {
+                                        saveClick();
+                                    } else {
+                                        history.push({
+                                            pathname: '/login',
+                                            state: {
+                                                redirectID: props.movie._id,
+                                            },
+                                        });
+                                    }
+                                }}
+                            >
+                                {isSaved === true ? '✓ Saved' : '+ For Later'}
+                            </button>
+                            <button
+                                className={
+                                    isLiked === true
+                                        ? 'btn btn-danger'
+                                        : 'btn btn-outline-danger'
+                                }
+                                onClick={() => {
+                                    if (user.currentUser) {
+                                        likeClick();
+                                    } else {
+                                        history.push({
+                                            pathname: '/login',
+                                            state: {
+                                                redirectID: props.movie._id,
+                                            },
+                                        });
+                                    }
+                                }}
+                            >
+                                {isLiked === true ? 'Liked' : 'Like'}
+                            </button>
+                        </div>
+                    </Card.Footer>
+                </Card>
+            </div>
+        </CSSTransition>
+    ) : (
+        <div></div>
+    );
+}
